aea57cd0a6705c4f69d489df27329a8b330b6f2c
pozhara <poliazhara04@gmail.com>
2023-03-12 13:50:46 +0000

    Add a greeting

--- run.py
+++ run.py
@@ -8,13 +8,23 @@ import random  # to approve or disapprove requests for time off
 import gspread  # to use google sheets
 from google.oauth2.service_account import Credentials
 
+# Defines the scope
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
 
+"""
+Adds credentials to the account and authorises the client sheet.
+Code taken from Love Sandwiches.
+"""
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("bank-job")
+
+# A greeting, instruction on what to do next
+print("Hello, we are a recently opened bank,")
+print("thank you for starting your career with us.")
+print("Your next step is to add yourself as an employee to our system.\n")
