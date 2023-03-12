9d12fa637db18c96b85eefa71248b342dd6c74f6
pozhara <poliazhara04@gmail.com>
2023-03-12 13:49:30 +0000

    Import needed libraries

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import datetime  # to check age
+import math  # to check decimal
+import sys  # to exit the program
+import time  # to add pauses
+from os import system  # to clear terminal
+import re  # to check for special characters in a string input
+import random  # to approve or disapprove requests for time off
+import gspread  # to use google sheets
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("bank-job")
